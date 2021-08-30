a32aed19fe15f7a03021c92ee8ecea94f1066df9
G11_Kulthoom <kulthumradi94@gmail.com>
2021-08-31 00:19:00 +0300

    w09d02 solution

--- react_blog/src/App.js
+++ react_blog/src/App.js
@@ -1,13 +1,18 @@
 import React, { useState, useEffect } from "react";
 import './App.css';
-
+import axios from "axios";
 function App() {
-
+  useEffect(()=>{
+    axios.get(`https://jsonplaceholder.typicode.com/posts`).then((result)=>{
+      setPosts([...posts,...result.data])
+    })
+    },[])
   const [posts, setPosts] = useState([{userId: 8,id:110  , title: "commeting" , body:"i love commints"},{userId: 6,id: 120  , title: "flying" , body:"i love to fly "}]);
  const [userId , setUserId] =  useState("")
  const [id , setId] =  useState("")
  const [title , setTitle] =  useState("")
  const [body , setBody] =  useState("")
+
   return (
 
     <>
